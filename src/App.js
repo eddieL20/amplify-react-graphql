7892ae20134182a97f37d7c86e6cfebc3e2f9b7b
Eduardo Lopez <eddie56.el@gmail.com>
2024-04-30 00:31:10 -0700

    remove Storage from import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { Amplify, Storage } from "aws-amplify";
+import { Amplify } from "aws-amplify";
 import {
   Button,
   Flex,
